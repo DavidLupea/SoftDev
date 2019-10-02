398b20ebe17f61a28db9235b3b0611c820c62fc0
DavidLupea <dlupea00@stuy.edu>
2019-10-02 09:28:52 -0400

    updated

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request
 
+username = "user"
+password = "admin"
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("/template.html")
+    return render_template("/landing.html")
 
 @app.route("/auth")
 def authenticate():
-    return "hi"
+    return render_template()
 
 
 if __name__ == "__main__":
