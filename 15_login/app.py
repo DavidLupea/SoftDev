e2113a842c42bbd4161c5fc4627073324c2913f6
DavidLupea <37593883+DavidLupea@users.noreply.github.com>
2019-10-14 11:22:33 -0400

    Update

--- 15_login/app.py
+++ 15_login/app.py
@@ -3,7 +3,6 @@
 # 2019-10-03
 
 import os
-import csv
 from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
@@ -13,19 +12,19 @@ creds = {"user": "user", "password": "password"}
 
 @app.route('/')  # Landing/Login
 def index():
-    if (session.get("user") == creds.get('user')) and (session.get("password") == creds.get('password')):   #Session info
+    if (session.get("user") == creds['user']) and (session.get("password") == creds['password']):   #Session info
         return redirect('/auth')
     return render_template('landing.html')
 
 @app.route("/auth")
 def authentication():
-    if request.args.get('user'):
-        session['user'] = request.args.get('user')  #Creates a session
-        session['password'] = request.args.get('pass')
+    if request.args['user']:
+        session['user'] = request.args['user']
+        session['password'] = request.args['pass']
     if 'user' in session:
-        if session.get("user") == creds.get('user') and session.get("password") == creds.get('password'):   #Load logged in page with user's information
+        if session["user"] == creds['user'] and session.get("password") == creds['password']:   #Load logged in page with user's information
             return render_template("response.html", info=session, method_type=request.method)
-        if session.get('password') != creds.get('password'):
+        if session.get('password') != creds['password']:
             return render_template("landing.html", error = "Your password is WRONG")
         return render_template("landing.html", error = "Your username is WRONG")
 
