bdb55f79e4372d1e6a1a051c7d43d7bb62481921
davidlupea <dlupea00@stuy.edu>
2019-11-13 09:17:59 -0500

    update

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,3 +1,22 @@
-import urllib2
-u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=guzL1fAp3pwbQ3bozJgEJTo6C5uJRxh8a5oQqEMN")
-print(u)
+#David Lupea
+#SoftDev1 pd2
+#K24 -- A RESTful Journey Skyward
+#2019-11-13
+
+from flask import Flask
+from flask import render_template
+from urllib.request import urlopen
+import json
+
+app = Flask(__name__)   #create instance of class Flask
+
+@app.route("/")
+def root():
+    u = urlopen("https://api.nasa.gov/planetary/apod?api_key=guzL1fAp3pwbQ3bozJgEJTo6C5uJRxh8a5oQqEMN")
+    response = u.read()
+    data = json.loads(response)
+    return render_template("index.html",pic = data['url'],capt = data['explanation'])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
