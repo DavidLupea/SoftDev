2b710ed6703d4601c4bc12cf53235f18df41f907
David <dlupea00@stuy.edu>
2019-12-12 22:02:41 -0500

    29

--- 28_jsdom/funky_town.js
+++ 28_jsdom/funky_town.js
@@ -1,28 +1,28 @@
 //factorial function
 var fact = function(n) {
-  if (n > 1){
-    return n * fact(n-1);
-  }
+    if (n > 1){
+        return n * fact(n-1);
+    }
     return 1;
 }
 
 //fibonocci function
 var fib = function(n) {
-  if (n == 0) {
-    return 0;
-  }
-  if (n > 2) {
-    return fib(n-1) + fib(n-2);
-  }
-  return 1;
+    if (n == 0) {
+        return 0;
+    }
+    if (n > 2) {
+      return fib(n-1) + fib(n-2);
+    }
+    return 1;
 }
 
 // gcd(a,b) returns the greatest common divisor of the input numbers a and b
 var gcd = function(a,b) {
-  if (a == 0) {
-    return b;
+    if (a == 0) {
+        return b;
   }
-  return gcd(b % a, a)
+    return gcd(b % a, a)
 }
 
 // array of students in SoftDev period 2
@@ -32,17 +32,17 @@ var studentsInPeriod2 = ["abedinM", "avrahamiB", "bhuiyanS", "caoW", "chanM", "c
 
 // randomStudent() returns a random student from the array studentsInPeriod2
 var randomStudent = function() {
-  randomNumber = Math.floor(Math.random() * studentsInPeriod2.length)
-  return studentsInPeriod2[randomNumber]
+    randomNumber = Math.floor(Math.random() * studentsInPeriod2.length)
+    return studentsInPeriod2[randomNumber]
 }
 //-----------------------------------------------------------------------------
 //PRINTING fact(5)
 var test1 = document.getElementById("a");
 function firstCons1() {
-  console.log(fact(5));
+    console.log(fact(5));
 }
 function firstPage1() {
-  document.write(fact(5));
+    document.write(fact(5));
 }
 test1.addEventListener("click", firstCons1);
 test1.addEventListener("click", firstPage1);
@@ -50,10 +50,10 @@ test1.addEventListener("click", firstPage1);
 //PRINTING fib(5)
 var test2 = document.getElementById("b");
 function firstCons2() {
-  console.log(fib(5));
+    console.log(fib(5));
 }
 function firstPage2() {
-  document.write(fib(5));
+    document.write(fib(5));
 }
 test2.addEventListener("click", firstCons2);
 test2.addEventListener("click", firstPage2);
@@ -61,10 +61,10 @@ test2.addEventListener("click", firstPage2);
 //PRINTING gcd(10,15)
 var test3 = document.getElementById("c");
 function firstCons3() {
-  console.log(gcd(10,15));
+    console.log(gcd(10,15));
 }
 function firstPage3() {
-  document.write(gcd(10,15));
+    document.write(gcd(10,15));
 }
 test3.addEventListener("click", firstCons3);
 test3.addEventListener("click", firstPage3);
@@ -73,10 +73,10 @@ test3.addEventListener("click", firstPage3);
 var test4 = document.getElementById("d");
 var ans = randomStudent();
 function firstCons4() {
-  console.log(ans);
+    console.log(ans);
 }
 function firstPage4() {
-  document.write(ans);
+    document.write(ans);
 }
 test4.addEventListener("click", firstCons4);
 test4.addEventListener("click", firstPage4);
