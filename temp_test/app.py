1c0a7296a709167da2fac5eb8faa7557ffc2bad7
unknown <dlupea00@stuy.edu>
2020-03-17 18:40:02 -0400

    11

--- temp_test/app.py
+++ temp_test/app.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, request
+from bson.json_util import loads
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017);
+try:
+    client.drop_database('frootNinjas')
+except:
+    pass
+
+db = client.frootNinjas
+collection = db.meteors
+
+if (collection.count() == 0):
+    f = open("meteor.json", "r")
+    data = f.readlines()
+    for line in data:
+        collection.insert_one(loads(line))
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    print(app)
-    return render_template("/form.html")
+def home():
+    return render_template("form.html")
 
 @app.route("/auth")
 def authenticate():
-    print(app)
-    print(request)
-    print(request.args)
-    return "hi"
+    return render_template("result.html", ans = "test1234")
 
 
 if __name__ == "__main__":
