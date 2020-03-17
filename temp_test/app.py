f598b2740e6cd12d9f958fa253c991efe553179b
unknown <dlupea00@stuy.edu>
2020-03-17 19:06:53 -0400

    11_6

--- temp_test/app.py
+++ temp_test/app.py
@@ -25,35 +25,35 @@ def home():
 
 @app.route("/name_to_mass")
 def name_to_mass():
-    return render_template("form.html", ans = request.args["input"])
+    return render_template("form.html", ans = findMass(request.args["input"]))
 
 @app.route("/name_to_year")
 def name_to_year():
-    return render_template("form.html", ans = "test1234")
+    return render_template("form.html", ans = findYear(request.args["input"]))
 
 @app.route("/name_to_class")
 def name_to_class():
-    return render_template("form.html", ans = "test1234")
+    return render_template("form.html", ans = findClass(request.args["input"]))
 
 @app.route("/name_to_location")
 def name_to_location():
-    return render_template("form.html", ans = "test1234")
+    return render_template("form.html", ans = findLocation(request.args["input"]))
 
 @app.route("/id_to_mass")
 def id_to_mass():
-    return render_template("form.html", ans = "test1234")
+    return render_template("form.html", ans = findMassbyID(request.args["input"]))
 
 @app.route("/id_to_year")
 def id_to_year():
-    return render_template("form.html", ans = "test1234")
+    return render_template("form.html", ans = findYearbyID(request.args["input"]))
 
 @app.route("/id_to_class")
 def id_to_class():
-    return render_template("form.html", ans = "test1234")
+    return render_template("form.html", ans = findClassbyID(request.args["input"]))
 
 @app.route("/id_to_location")
 def id_to_location():
-    return render_template("form.html", ans = "test1234")
+    return render_template("form.html", ans = findLocationbyID(request.args["input"]))
 
 
 def findMass(name):
@@ -74,23 +74,35 @@ def findYear(name):
     for result in results:
         return result["year"]
 def findMassbyID(id):
-    id = str(id)
+    try:
+        id = str(id)
+    except:
+        return "Integers only!!!"
     results = collection.find({"id" : id})
     for result in results:
         return result["mass"]
 def findLocationbyID(id):
-    id = str(id)
+    try:
+        id = str(id)
+    except:
+        return "Integers only!!!"
     results = collection.find({"id": id})
     for result in results:
         ans = result["reclat"] + " " + result["reclong"]
         return ans
 def findClassbyID(id):
-    id = str(id)
+    try:
+        id = str(id)
+    except:
+        return "Integers only!!!"
     results = collection.find({"id" : id})
     for result in results:
         return result["recclass"]
 def findYearbyID(id):
-    id = str(id)
+    try:
+        id = str(id)
+    except:
+        return "Integers only!!!"
     results = collection.find({"id" : id})
     for result in results:
         return result["year"]
